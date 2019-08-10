69a0375f229f8821252b012b0785fd5322210cfa
Max Turner <mhturner@stanford.edu>
2019-08-09 21:46:34 -0700

    init order was causing improper import of get_n_z_layers. Fixed now.

--- visanalysis/volume_tools/__init__.py
+++ visanalysis/volume_tools/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .create_bruker_objects_from_zstack import create_bruker_objects_from_zstack
-
-from .take_every_other_frame import take_every_other_frame
 from .get_n_z_layers import get_n_z_layers
+from .create_bruker_objects_from_zstack import create_bruker_objects_from_zstack
+from .take_every_other_frame import take_every_other_frame
