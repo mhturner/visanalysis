ff6f653cdbf844e70588ad112cc8ab41ed743731
MINSEUNG CHOI <minsung0315@gmail.com>
2019-08-22 13:24:40 -0700

    jk

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='visanalysis_volume',
+    name='visanalysis',
     version='0.1.0',
     description='Analysis environment for flystim experiments',
     url='https://github.com/ClandininLab/visanalysis',
     author='Max Turner',
     author_email='mhturner@stanford.edu',
-    packages=['visanalysis_volume'],
+    packages=['visanalysis'],
     install_requires=[
         'PyQT5',
         'numpy',
